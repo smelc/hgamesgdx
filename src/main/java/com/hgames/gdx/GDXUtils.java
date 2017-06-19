413e7431fc6091823b9d6b44ec3f2a9d6215a350
smelc <hgamesdeveloper@gmail.com>
2017-06-19 11:05:14 +0200

    Add drawRectangle and drawRectangleFrame to GDXUtils

--- src/main/java/com/hgames/gdx/GDXUtils.java
+++ src/main/java/com/hgames/gdx/GDXUtils.java
@@ -3,6 +3,7 @@ package com.hgames.gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 
 /**
  * @author smelC
@@ -21,6 +22,48 @@ public class GDXUtils {
 		texture.draw(tmp, 0, 0);
 		tmp.dispose();
 		return texture;
+	}
+
+	/**
+	 * Draw {@code texture} within the rectangle at (x,y) with the given width
+	 * and height.
+	 * 
+	 * @param batch
+	 * @param texture
+	 * @param color
+	 * @param x
+	 * @param y
+	 * @param width
+	 * @param height
+	 */
+	public static void drawRectangle(Batch batch, Texture texture, /* @Nullable */ Color color, float x,
+			float y, float width, float height) {
+		final Color save = batch.getColor();
+		if (color != null)
+			batch.setColor(color);
+		batch.draw(texture, x, y, width, height);
+		batch.setColor(save);
+	}
+
+	/**
+	 * @param batch
+	 * @param texture
+	 * @param color
+	 * @param x
+	 * @param y
+	 * @param width
+	 * @param height
+	 */
+	public static void drawRectangleFrame(Batch batch, Texture texture, /* @Nullable */ Color color, float x,
+			float y, float width, float height) {
+		/* Bottom line */
+		drawRectangle(batch, texture, color, x, y, width, 1f);
+		/* Top line */
+		drawRectangle(batch, texture, color, x, y + height, width, 1);
+		/* Left line */
+		drawRectangle(batch, texture, color, x, y, 1, height);
+		/* Right line */
+		drawRectangle(batch, texture, color, x + width, y, 1, height);
 	}
 
 }
