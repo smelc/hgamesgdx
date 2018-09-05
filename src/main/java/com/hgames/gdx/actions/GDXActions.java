1cce31e0d1ec22091f790ed64f068039d8bc08f4
smelc <hgamesdeveloper@gmail.com>
2018-09-05 14:52:45 +0200

    detail: Export tinting duration ratios in GDXActions

--- src/main/java/com/hgames/gdx/actions/GDXActions.java
+++ src/main/java/com/hgames/gdx/actions/GDXActions.java
@@ -12,6 +12,11 @@ import com.badlogic.gdx.scenes.scene2d.actions.Actions;
  */
 public class GDXActions {
 
+	/** Percentage of duration to go to alpha 1 when tinting */
+	public static final float TINT_GO_RATIO = 0.3f;
+	/** Percentage of duration to go to alpha 0 when tinting */
+	public static final float TINT_BACK_RATIO = (1 - TINT_GO_RATIO);
+
 	/**
 	 * @param tinted
 	 * @param color
@@ -19,8 +24,8 @@ public class GDXActions {
 	 * @return A tint action.
 	 */
 	public static Action tintAction(Actor tinted, Color color, float duration) {
-		final Action go = Actions.color(color, duration * 0.3f);
-		final Action back = Actions.color(tinted.getColor(), duration * 0.7f);
+		final Action go = Actions.color(color, duration * TINT_GO_RATIO);
+		final Action back = Actions.color(tinted.getColor(), duration * TINT_BACK_RATIO);
 		final Action action = Actions.sequence(go, back);
 		return action;
 	}
